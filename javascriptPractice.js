7ddb461c0440b80892a84caee86f6ca45da7b1e4
milosdj13 <milosdjurica13@gmail.com>
2022-05-10 00:57:54 +0200

    day2 js 7kyu

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1,7 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                    // 7kyu
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 // The binary number returned should be a string.
-// Examples:(Input1, Input2 --> Output (explanation)))
 
+// Examples:(Input1, Input2 --> Output (explanation)))
 // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
@@ -11,7 +19,111 @@ function addBinary(a, b) {
 
 
 
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum( a,b ){
+    // range (1, 100)
+    // (99+1)*(101)/2 = 50*101
+    return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+//  The Math.ceil() function always rounds a number up to the next largest integer.
+    return Math.ceil(year/100)
+}
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+    return name.charAt(0).toLowerCase() === "r"  ? `${name} plays banjo` : `${name} does not play banjo`
+}
+
+
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+
+// Input constraints:
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+// const past = (h,m,s) => 1000 * (3600 * h + 60 * m + s);
+function past(h, m, s){
+    return h*60*60*1000 + m*60*1000 + s*1000
+}
+
+
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+
+    let arr = s1.concat(s2).split('')   
+    // transformating to Set to get DISTINCTED values
+    let set = new Set(arr)    
+    
+    return Array.from(set).sort().join('')     //   1 line solution also work
+    // return Array.from(new Set(s1.concat(s2).split(''))).sort().join('')
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                    // 6kyu
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
