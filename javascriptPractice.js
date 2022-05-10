8eafea31b9707eb8e3111678f0e07dd5c8460a04
milosdj13 <milosdjurica13@gmail.com>
2022-05-11 00:24:22 +0200

    day3 js 7kyu

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -112,12 +112,60 @@ function longest(s1, s2) {
 
 
 
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+    return numbers.reduce((pre, cur) => pre + cur**2, 0)
+}
+
+
+
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+const numberToString = num => num.toString()
+
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+const even_or_odd = number => number%2==0  ? "Even" : "Odd"
 
 
 
 
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
 
+// Examples
+// * Input: [1,2,3,4,5], output= [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
 
+function removeSmallest(numbers) {
+    let min = Math.min(...numbers)
+    let found = false
+    let solution= []
+  
+    for(let i=0; i<numbers.length; i++){
+      if(numbers[i] !== min || found){
+        solution.push(numbers[i])
+      }else{
+        found = true
+      }
+    }
+    return solution
+}
+// good solution, not mine
+// const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
 
 
 
