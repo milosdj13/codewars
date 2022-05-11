2c30e06ee61a3de047f364d6f776b55e1bab2b51
milosdj13 <milosdjurica13@gmail.com>
2022-05-11 02:19:55 +0200

    day3 js 6kyu

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1,11 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                                    // 7kyu
+// 7kyu
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+// 1
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 // The binary number returned should be a string.
 
@@ -18,7 +18,7 @@ function addBinary(a, b) {
 }
 
 
-
+// 2
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 // Note: a and b are not ordered!
 
@@ -30,14 +30,14 @@ function addBinary(a, b) {
 // (-1, 0) --> -1 (-1 + 0 = -1)
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 
-function getSum( a,b ){
+function getSum(a, b) {
     // range (1, 100)
     // (99+1)*(101)/2 = 50*101
-    return (Math.abs(a - b) + 1) * (a+b) / 2;
+    return (Math.abs(a - b) + 1) * (a + b) / 2;
 }
 
 
-
+// 3
 // Introduction
 // The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
 // Task
@@ -50,12 +50,12 @@ function getSum( a,b ){
 // 2000 --> 20
 
 function century(year) {
-//  The Math.ceil() function always rounds a number up to the next largest integer.
-    return Math.ceil(year/100)
+    //  The Math.ceil() function always rounds a number up to the next largest integer.
+    return Math.ceil(year / 100)
 }
 
 
-
+// 4
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo!
 
@@ -65,11 +65,11 @@ function century(year) {
 // Names given are always valid strings.
 
 function areYouPlayingBanjo(name) {
-    return name.charAt(0).toLowerCase() === "r"  ? `${name} plays banjo` : `${name} does not play banjo`
+    return name.charAt(0).toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo`
 }
 
 
-
+// 5
 // Clock shows h hours, m minutes and s seconds after midnight.
 // Your task is to write a function which returns the time since midnight in milliseconds.
 
@@ -85,12 +85,12 @@ function areYouPlayingBanjo(name) {
 // 0 <= s <= 59
 
 // const past = (h,m,s) => 1000 * (3600 * h + 60 * m + s);
-function past(h, m, s){
-    return h*60*60*1000 + m*60*1000 + s*1000
+function past(h, m, s) {
+    return h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000
 }
 
 
-
+// 6
 // Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 // Examples:
 // a = "xyaabbbccccdefww"
@@ -102,26 +102,26 @@ function past(h, m, s){
 
 function longest(s1, s2) {
 
-    let arr = s1.concat(s2).split('')   
+    let arr = s1.concat(s2).split('')
     // transformating to Set to get DISTINCTED values
-    let set = new Set(arr)    
-    
+    let set = new Set(arr)
+
     return Array.from(set).sort().join('')     //   1 line solution also work
     // return Array.from(new Set(s1.concat(s2).split(''))).sort().join('')
 }
 
 
-
+// 7
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-function squareSum(numbers){
-    return numbers.reduce((pre, cur) => pre + cur**2, 0)
+function squareSum(numbers) {
+    return numbers.reduce((pre, cur) => pre + cur ** 2, 0)
 }
 
 
-
+// 8
 // We need a function that can transform a number into a string.
 // What ways of achieving this do you know?
 
@@ -131,13 +131,13 @@ function squareSum(numbers){
 const numberToString = num => num.toString()
 
 
-
+// 9
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-const even_or_odd = number => number%2==0  ? "Even" : "Odd"
+const even_or_odd = number => number % 2 == 0 ? "Even" : "Odd"
 
 
 
-
+// 10
 // The museum of incredible dull things
 // The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
 // However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
@@ -153,14 +153,14 @@ const even_or_odd = number => number%2==0  ? "Even" : "Odd"
 function removeSmallest(numbers) {
     let min = Math.min(...numbers)
     let found = false
-    let solution= []
-  
-    for(let i=0; i<numbers.length; i++){
-      if(numbers[i] !== min || found){
-        solution.push(numbers[i])
-      }else{
-        found = true
-      }
+    let solution = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] !== min || found) {
+            solution.push(numbers[i])
+        } else {
+            found = true
+        }
     }
     return solution
 }
@@ -169,9 +169,127 @@ function removeSmallest(numbers) {
 
 
 
+// 11
+// Task
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+function SeriesSum(n) {
+    let sum = 0;
+    let div = 1;
+    for (let i = 0; i < n; i++) {
+        sum = sum + 1 / div
+        div = div + 3
+    }
+    return sum.toFixed(2)
+}
+
+
+// 12
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+const makeNegative = num => num > 0 ? -num : num
+
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                                    // 6kyu
+// 6kyu
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// 13
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parentheses!
+function createPhoneNumber(numbers) {
+    let solution = ``
+    for (let i = 0; i < numbers.length; i++) {
+        if (i === 0) {
+            solution += `(${numbers[i]}`
+        } else if (i === 2) {
+            solution += `${numbers[i]}) `
+        } else if (i == 5) {
+            solution += `${numbers[i]}-`
+        } else {
+            solution += `${numbers[i]}`
+        }
+    }
+    return solution
+}
+
+
+
+// 14
+// A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+// For example, take 153 (3 digits), which is narcisstic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+function narcissistic(value) {
+    let arr = value.toString().split('')
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i] ** arr.length
+    }
+
+    if (sum === value) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+// 15
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+const alphabetPosition = text => text.toLowerCase().split('').filter((el) => el >= "a" && el <= "z").map((el) => el.charCodeAt(0) - 96).join(' ')
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                    // 5kyu
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
