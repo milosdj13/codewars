be1b2a2bb321de88a7d5a86311559de79ffbe22e
milosdj13 <milosdjurica13@gmail.com>
2022-05-15 18:20:35 +0200

    day 7, java13, js 18, completed kata 138

--- javaPractice.java
+++ javaPractice.java
@@ -189,6 +189,72 @@ public class Calculate {
 }
 
 
+// 11
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+public class Sid {
+    public static int simpleMultiplication(int n) {
+      return n%2==1 ? n*9 : n*8;
+    }
+}
+
+
+
+// 12
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+public class Kata {
+  public static String rps(String p1, String p2) {
+
+    if(p1=="scissors" && p2=="paper"){
+      return "Player 1 won!";
+    }
+    if(p1=="paper" && p2=="rock"){
+      return "Player 1 won!";
+    }
+    if(p1=="rock" && p2=="scissors"){
+      return "Player 1 won!";
+    }
+    if(p1.equals(p2)){
+      return "Draw!";
+    }
+    return "Player 2 won!";
+  }
+}
+
+
+
+
+// 13
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    return op.equals("+") ? v1+v2 : op.equals("-") ? v1-v2 : op.equals("*") ? v1*v2 : v1/v2;
+  }
+}
+
+
+
+
+
+
+
+
+
 
 
 
