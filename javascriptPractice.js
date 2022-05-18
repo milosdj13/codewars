9da3a107c86e1022ca9938a6a1e2be4c7fe0729d
milosdj13 <milosdjurica13@gmail.com>
2022-05-18 04:41:49 +0200

    day 10, java20, js28, honor240

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -492,7 +492,42 @@ function solution(str, ending) {
 
 
 
+// 27
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+// Based on: http://oj.leetcode.com/problems/two-sum/
 
+// twoSum [1, 2, 3] 4 === (0, 2)
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
+// 28
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+function expandedForm(num) {
+    return num.toString().split('').reverse().map((el, index) => {
+        if (el != '0') {
+            return `${el * (10 ** index)} + `
+        }
+    }).reverse().join('').slice(0, -3)
+}
 
 
 
