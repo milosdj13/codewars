fcff843f0a8bd4ac9d09712352be2b9bd17fbb34
milosdj13 <milosdjurica13@gmail.com>
2022-06-01 10:41:59 +0200

    day24, js61, py36, java30, honor483, 5kyu

--- pythonPractice.py
+++ pythonPractice.py
@@ -577,3 +577,45 @@ def find_difference(a, b):
     return abs(first-second)
 
 
+
+
+
+
+# 35
+# Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+# Example: (Input1, Input2 -->Output)
+# "4",  "5" --> "9"
+# "34", "5" --> "39"
+# "", "" --> "0"
+# "2", "" --> "2"
+# "-5", "3" --> "-2"
+
+# Notes:
+# If either input is an empty string, consider it as zero.
+# Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+def sum_str(a, b):
+    if a == "":
+        a = 0
+    if b == "":
+        b = 0
+        
+    return str(int(a) + int(b))
+   
+    
+
+
+
+
+
+# 36
+# Now you have to write a function that takes an argument and returns the square of it.
+def square(n):
+    return n**2
+
+
+
+
+
+
+
+
