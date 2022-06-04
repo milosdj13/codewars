b7ac4ba4511862606fbf350d2839faee9e015d82
milosdj13 <milosdjurica13@gmail.com>
2022-06-05 00:57:38 +0200

    day27, js74, py38, java31, honor527, 5kyu

--- javaPractice.java
+++ javaPractice.java
@@ -654,6 +654,25 @@ public class GrassHopper {
 
 
 
+// 31
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+// LaLiga, Copa del Rey, Champions League
+// Complete the function to return his total number of goals in all three leagues.
+// Note: the input will always be valid.
+
+// For example:
+// 5, 10, 2  -->  17
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+       return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
+
+
+
+
+
 
 
 
--- pythonPractice.py
+++ pythonPractice.py
@@ -642,3 +642,27 @@ def is_valid_walk(walk):
 
 
 
+# 38
+# A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+# He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+# His mother looks out of a window 1.5 meters from the ground.
+# How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+# Three conditions must be met for a valid experiment:
+# Float parameter "h" in meters must be greater than 0
+# Float parameter "bounce" must be greater than 0 and less than 1
+# Float parameter "window" must be less than h.
+# If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+def bouncing_ball(h, bounce, window):
+    if h>0 and bounce >0 and bounce < 1 and window<h:
+        count = 0
+        while h>window:
+            count = count + 1
+            h = h*bounce
+            if h>window:
+                count = count + 1
+        return count
+    return -1
+
+
+
