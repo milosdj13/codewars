107490642d338ef5ddc0fe0505174854718c3f22
milosdj13 <milosdjurica13@gmail.com>
2022-06-15 00:07:50 +0200

    day37, js100, py42, java32, honor579, 5kyu

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1837,6 +1837,43 @@ function pillars(num_pill, dist, width) {
 
 
 
+// 99
+// This program tests the life of an evaporator containing a gas.
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+// Example:
+// evaporator(10, 10, 5) -> 29
+// Note:
+// Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+function evaporator(content, evap_per_day, threshold) {
+    let count = 0
+    threshold = content * threshold / 100
+    while (content >= threshold) {
+        content -= content * evap_per_day / 100
+        count++
+    }
+    return count
+}
+
+
+
+
+
+
+// 100
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+function getSize(width, height, depth) {
+    let area = 2 * (width * height + width * depth + height * depth)
+    let volume = width * height * depth
+    return [area, volume]
+}
+
+
+
+
+
+
 
 
 
