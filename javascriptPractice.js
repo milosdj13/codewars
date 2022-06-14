0f387d36a4edf741db6dfc4fdac046a5c31b8ee8
milosdj13 <milosdjurica13@gmail.com>
2022-06-14 15:50:49 +0200

    day36, js98, py42, java32, honor575, 5kyu

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1809,3 +1809,34 @@ function points(games) {
 
 
 
+
+// 98
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+function pillars(num_pill, dist, width) {
+    if (num_pill < 2) {
+        return 0
+    }
+    dist = dist * 100
+    if (num_pill === 2) {
+        return dist
+    }
+    let sum = 0
+    for (let i = 1; i < num_pill; i++) {
+        sum += dist + width
+    }
+
+    return sum - width
+
+}
+
+
+
+
+
+
+
+
