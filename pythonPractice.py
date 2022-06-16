5e993f6e38a84a6f8306ae245035557c395d28e8
milosdj13 <milosdjurica13@gmail.com>
2022-06-17 01:07:54 +0200

    day39, js102, py46, java32, honor591, 5kyu

--- pythonPractice.py
+++ pythonPractice.py
@@ -790,3 +790,62 @@ def hello(name=''):
 
 
 
+
+# 45
+# Complete the function that receives as input a string, and produces outputs according to the following table:
+
+# Input	Output
+# "Jabroni"	"Patron Tequila"
+# "School Counselor"	"Anything with Alcohol"
+# "Programmer"	"Hipster Craft Beer"
+# "Bike Gang Member"	"Moonshine"
+# "Politician"	"Your tax dollars"
+# "Rapper"	"Cristal"
+# anything else	"Beer"
+# Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+# Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+def get_drink_by_profession(param):
+    drinks = {
+        "jabroni" : "Patron Tequila",
+        "school counselor" : "Anything with Alcohol",
+        "programmer" : "Hipster Craft Beer",
+        "bike gang member" : "Moonshine", 
+        "politician" : "Your tax dollars",
+        "rapper" : "Cristal"
+    }
+    return drinks[param.lower()] if param.lower() in drinks else "Beer"
+   
+
+
+
+
+
+
+# # 46
+# Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+# Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+# Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+# "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+# Good Luck!
+def calculate_age(year_of_birth, current_year):
+    if year_of_birth == current_year+1:
+        return f"You will be born in {year_of_birth - current_year} year."
+    
+    if year_of_birth == current_year-1:
+        return f"You are {current_year - year_of_birth} year old."
+    
+    if year_of_birth < current_year:
+        return f"You are {current_year - year_of_birth} years old."
+    
+    if year_of_birth == current_year:
+        return "You were born this very year!"
+    
+    return f"You will be born in {year_of_birth - current_year} years."
+
+
+
+
+
+
+
+
