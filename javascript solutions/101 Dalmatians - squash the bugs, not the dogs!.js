687d588b274966de4526ecaafafdcf94b1fcd380
milosdjurica <milosdjurica13@gmail.com>
2023-09-07 18:03:41 +0200

    js216, honor1037, 4kyu

--- javascript solutions/101 Dalmatians - squash the bugs, not the dogs!.js
+++ javascript solutions/101 Dalmatians - squash the bugs, not the dogs!.js
@@ -1,4 +1,4 @@
-// 8kyu 
+// 8kyu
 
 // Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
 
@@ -9,21 +9,21 @@
 // Good luck!
 
 function howManyDalmatians(number) {
-	var dogs = [
-		"Hardly any",
-		"More than a handful!",
-		"Woah that's a lot of dogs!",
-		"101 DALMATIANS!!!",
-	];
+  var dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIANS!!!",
+  ];
 
-	var respond =
-		number <= 10
-			? dogs[0]
-			: number <= 50
-			? dogs[1]
-			: number < 101
-			? dogs[2]
-			: dogs[3];
+  var respond =
+    number <= 10
+      ? dogs[0]
+      : number <= 50
+      ? dogs[1]
+      : number < 101
+      ? dogs[2]
+      : dogs[3];
 
-	return respond;
+  return respond;
 }
