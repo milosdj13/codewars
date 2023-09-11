bc7f23530efbf75be56357e7f7ad0744622a586b
milosdjurica <milosdjurica13@gmail.com>
2023-09-11 17:26:10 +0200

    js217, honor1045, 4kyu

---
+++ javascript solutions/Moving Zeros To The End.js
@@ -0,0 +1,24 @@
+// !5kyu
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+  let zeros = [];
+  let noZeros = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i]);
+    } else {
+      noZeros.push(arr[i]);
+    }
+  }
+  return noZeros.concat(zeros);
+}
+
+// !ONE LINE SOLUTION
+function moveZeros(arr) {
+  return arr.filter((el) => el !== 0).concat(arr.filter((el) => el === 0));
+}
