c9dd120427870d4b48036890d3fb8ff89456e6c0
milosdjurica <milosdjurica13@gmail.com>
2023-09-17 15:38:58 +0200

    java42, kata1069, 4kyu

---
+++ java solutions/SumOfDifArray.java
@@ -0,0 +1,31 @@
+// 8kyu Sum of differences in array
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+public class SumOfDifArray {
+    public static int sumOfDifferences(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            count += arr[i] - arr[i + 1];
+        }
+        return count;
+    }
+}
